0b2b500fac0cd6e93c22c32e34c7f0afcefc5e3f
snehakadham-sf <sneha.kadham@sharefile.com>
2025-08-14 08:47:16 -0400

    snehakadham 158
    158 non-whitespace characters

--- code_golf.py
+++ code_golf.py
@@ -6,7 +6,7 @@ for s in open(sys.argv[1]):
     f=''
     while n>0:
         j=n%10
-        f=(z[p*2]+(z[p*2+j//4]) if j in [4,9] else z[p*2+1]*(j//5)+z[p*2]*(j%5))+f
+        f=(z[p*2]+(z[p*2+j//4]) if j%5==4 else z[p*2+1]*(j//5)+z[p*2]*(j%5))+f
         n//=10
         p+=1
     print(f)
