8c2df0e3b62094ef83f5a40feb604c7284d1b5fc
snehakadham-sf <sneha.kadham@sharefile.com>
2025-08-13 21:34:26 -0400

    snehakadham 160
    160 non-whitespace characters

--- code_golf.py
+++ code_golf.py
@@ -1,16 +1,12 @@
 import sys
 z='IVXLCDM '
-with open(sys.argv[1], 'r') as c:
-    for s in c:
-        n=int(s)
-        p=0
-        f=''
-        while n>0:
-            j=n%10
-            if j in [4,9]:
-                f=z[p*2]+(z[p*2+(1 if j==4 else 2)])+f
-            else:
-                f=z[p*2+1]*(j//5)+z[p*2]*(j%5)+f
-            n//=10
-            p+=1
-        print(f)
+for s in open(sys.argv[1]):
+    n=int(s)
+    p=0
+    f=''
+    while n>0:
+        j=n%10
+        f=(z[p*2]+(z[p*2+j//4]) if j in [4,9] else z[p*2+1]*(j//5)+z[p*2]*(j%5))+f
+        n//=10
+        p+=1
+    print(f)
